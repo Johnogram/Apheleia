f316257e4a1d0c33713397d7b1344afbd0ee4223
John Minns <john@john-o-gram.co.uk>
2018-02-03 18:48:30 +0000

    Update URLs

--- apheleia.php
+++ apheleia.php
@@ -14,11 +14,11 @@
  *
  * @wordpress-plugin
  * Plugin Name:       Apheleia
- * Plugin URI:        https://github.com/Johnogram/Apheleia
+ * Plugin URI:        https://johnogram.github.io/Apheleia/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
  * Author:            John-O-Gram
- * Author URI:        minns.io
+ * Author URI:        http://minns.io
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       apheleia
