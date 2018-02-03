985f3881b909126c4fccb8d81dfa0145a3f468b3
John Minns <john@john-o-gram.co.uk>
2018-02-03 20:56:33 +0000

    Proof of concept working
    Working poc, working with twentyseventeen theme

--- public/class-apheleia-public.php
+++ public/class-apheleia-public.php
@@ -110,39 +110,136 @@ class Apheleia_Public {
 		/**
 		 * Bail out if child theme is active
 		 * 
-		 * (Added to roadmap)
+		 * (ADDED TO ROADMAP)
 		 */
 		if ( $this->is_child_theme() )
 			return;
 
-		
+		/**
+		 * Get stylesheets content
+		 * 
+		 * Bail out if stylesheets aren't set
+		 */
+		if ( !$this->get_theme_styles_url() )
+			return;
+
+		foreach ( $this->get_theme_styles_url() as $key => $file ) {
+			$stylesheets[$key] = $this->get_stylesheet_content( $file );
+		}
+
+		/**
+		 * Output the stylesheet contents
+		 */
+		foreach ( $stylesheets as $stylesheet ) {
+			echo $this->format_inline_css( $stylesheet );
+		}
+
 	}
 
 	/**
 	 * Check if child theme is in use
 	 * 
 	 * @since 	1.0.0
+	 * @access 	private
+	 * @return	boolean		If child theme is in use
 	 */
 	private function is_child_theme() {
-
 		return is_child_theme();
 	}
 
 	/**
 	 * Get the theme stylesheet
 	 * 
+	 * (WILL BECOME ALL ENQUEUE STYLESHEETS)
+	 * 
 	 * @since	1.0.0
+	 * @access 	private
+	 * @return	string		$stylesheet_url		The stylesheets URL
 	 */
 	private function get_theme_styles_url() {
+
+		$stylesheet_url['theme'] = get_stylesheet_uri();
+		// return array for future proofing
+		return $stylesheet_url;
 
 	}
 
 	/**
-	 * Open and return the contents of stylesheet
+	 * Dequeue the stylesheets
+	 * 
+	 * (HARDCODED FOR NOW)
+	 * 
+	 * @since	1.0.0
+	 */
+	public function dequeue_stylesheets() {
+
+		wp_dequeue_style( 'twentyseventeen-style' );
+		wp_deregister_style( 'twentyseventeen-style' );
+		wp_dequeue_style( 'twentyseventeen-ie8' );
+		wp_deregister_style( 'twentyseventeen-ie8' );
+
+	}
+
+	/**
+	 * Open and return the contents of a stylesheet
 	 * 
 	 * @since 	1.0.0
+	 * @access 	private
+	 * @param 	string		$file	The URL of a stylesheet
+	 * @return 	string 		$file_contents 	The contents of the stylesheet
 	 */
-	private function get_theme_styles_content( $file ) {
+	private function get_stylesheet_content( $file ) {
+
+		$file_contents = file_get_contents( $file );
+
+		/**
+		 * Strip comments
+		 * Strip excess whitespace
+		 * Non-destructive CSS formatting
+		 * 
+		 * Credit: https://www.progclub.org/blog/2012/01/10/compressing-css-in-php-no-comments-or-whitespace/
+		 */
+		$replace = array(
+			"#/\*.*?\*/#s" 		=> "",  // Strip C style comments.
+			"#\s\s+#"      		=> " ", // Strip excess whitespace.
+			"/[\r|\n|\t|\r\n]/"	=> "", // Strip new lines, tabs, etc...
+		);
+		$search = array_keys($replace);
+		$file_contents = preg_replace($search, $replace, $file_contents);
+		
+		$replace = array(
+			": "  => ":",
+			"; "  => ";",
+			" {"  => "{",
+			" }"  => "}",
+			", "  => ",",
+			"{ "  => "{",
+			";}"  => "}", // Strip optional semicolons.
+			",\n" => ",", // Don't wrap multiple selectors.
+			"\n}" => "}", // Don't wrap closing braces.
+		);
+		$search = array_keys($replace);
+		$file_contents = str_replace($search, $replace, $file_contents);
+
+		return $file_contents;
+
+	}
+
+	/**
+	 * Return the inline CSS for the page head
+	 * 
+	 * @since	1.0.0
+	 * @access 	private
+	 * @param 	string		$css			The raw CSS to be inlined
+	 * @return 	string 		$inline_css 	The inline HTML to be output
+	 */
+	private function format_inline_css( $css ) {
+
+		$inline_css = '<style type="text/css">';
+		$inline_css .= $css;
+		$inline_css .= '</style>';
+
+		return $inline_css;
 
 	}
 
